9a37327f5d9a215c77c90de800a02997fe44d9e3
ci-mustafa akbari <akbarimustafa64@gmail.com>
2024-07-15 10:00:41 +0000

    feat: add single line arrow function and call it

--- arrow-functions.js
+++ arrow-functions.js
@@ -5,23 +5,35 @@
 
 // Vanilla JavaScript Function
 function addTwoNumber(a, b) {
-    return a + b
+    return a + b;
 }
 
 // call the function
-let myVanillaFunction = addTwoNumber(5, 6)
-console.log(myVanillaFunction)
+let myVanillaFunction = addTwoNumber(5, 6);
+console.log(myVanillaFunction);
 
+
+//-----------------------------------------------------------
 // Arrow Function With Parameters
 const subTwoNumber = (a, b) => {
-    return a - b
+    return a - b;
 }
 
 // call the function
-let myArrowFunction = subTwoNumber(4, 6)
-console.log(myArrowFunction)
+let myArrowFunction = subTwoNumber(4, 6);
+console.log(myArrowFunction);
+
+//-----------------------------------------------------------
 // Single Line Arrow Function With Parameters
+const mulTwoNumber = (a, b) => a * b;
+let singleLineArrowFunction = mulTwoNumber(3, 3)
+console.log(singleLineArrowFunction);
 
+//-----------------------------------------------------------
 // Implicit Returns
+const showMessage = message => console.log(message);
+showMessage("this is a message!");
 
+const sayHello = () => console.log("Hello!");
+sayHello();
 // Returning Multiple Lines
